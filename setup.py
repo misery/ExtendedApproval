ab8f5e813d7c21df8a0eafd43c82e197771c445f
André Klitzing <aklitzing@gmail.com>
2018-03-10 17:34:03 +0100

    Add information for pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from __future__ import unicode_literals
 from reviewboard.extensions.packaging import setup
 
 
-PACKAGE = "ExtendedApproval"
-VERSION = "0.6"
+PACKAGE = "rbExtendedApproval"
+VERSION = "0.6.1"
 
 setup(
     name=PACKAGE,
     version=VERSION,
-    description="Extension ExtendedApproval",
-    author="Andre Klitzing",
+    description='Review Board extension: ExtendedApproval',
+    author='Andre Klitzing',
+    author_email='aklitzing@gmail.com',
+    url='https://github.com/misery/ExtendedApproval',
     packages=[str('extended_approval')],
+    install_requires=[
+        'reviewboard>=3',
+    ],
     entry_points={
         'reviewboard.extensions':
             '%s = extended_approval.extension:ExtendedApproval' % PACKAGE,
@@ -21,5 +26,14 @@ setup(
             'templates/extended_approval/*.txt',
             'templates/extended_approval/*.html',
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
 )
