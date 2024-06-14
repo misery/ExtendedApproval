misery/ExtendedApproval

commit 7968a582351b81d504e6c01cea4eae73f1244f73

Fix deprecation warning for utcfromtimestamp
DeprecationWarning:
  datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version.
  Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).

contrib/mercurial_git_push.py
@@ -902,7 +902,7 @@ class MercurialRevision(BaseRevision):
 
             d = self.json['date']
             offset = d[1] * -1
-            d = dt.datetime.utcfromtimestamp(d[0] + offset)
+            d = dt.datetime.fromtimestamp(d[0] + offset, dt.UTC)
             d = d.replace(tzinfo=Offset(offset))
             self._date = d.isoformat(str(' '))
 

commit aab461217293808abf857afe971835aefb4f0f05

Fix another python2 issue
This was introduced by 7968a582351b81d504e6c01cea4eae73f1244f73.

contrib/mercurial_git_push.py
@@ -1139,7 +1139,10 @@ class MercurialRevision(BaseRevision):
 
             d = self.json['date']
             offset = d[1] * -1
-            d = dt.datetime.fromtimestamp(d[0] + offset, dt.UTC)
+            if six.PY2:
+                d = dt.datetime.utcfromtimestamp(d[0] + offset)
+            else:
+                d = dt.datetime.fromtimestamp(d[0] + offset, dt.UTC)
             d = d.replace(tzinfo=Offset(offset))
             self._date = d.isoformat(str(' '))
 
@@ -1808,7 +1811,7 @@ def set_options():
         HG_ENV_PREFIX = 'HG_USERVAR_'
         for key, value in six.iteritems(os.environ):
             if key.startswith(HG_ENV_PREFIX):
-                OPTIONS[key.removeprefix(HG_ENV_PREFIX)] = value
+                OPTIONS[key[len(HG_ENV_PREFIX):]] = value
 
 
 def set_topic_usage():